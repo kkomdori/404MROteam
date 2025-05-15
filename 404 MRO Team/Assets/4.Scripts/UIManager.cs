a876026bf39c3644332f2f6b20fe4a9b4ded6286
Manje0304 <rhakswp1@gmail.com>
2025-05-15 16:21:07 +0900

    무야호

--- 404 MRO Team/Assets/4.Scripts/UIManager.cs
+++ 404 MRO Team/Assets/4.Scripts/UIManager.cs
@@ -2,22 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UIManager : MonoBehaviour
 {
     public Animator image1;
     public Animator title;
-    public Animator tvFace;
+    public Animator tvFrame;
     public Animator image2;
     public Animator startBtn;
+
+    public List<GameObject> stageUIList;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OpenUis(true); // 무조건 true로 설정
+            CloseAllStageUI();
+            EventSystem.current.SetSelectedGameObject(null); // 선택된 UI 해제
+        }
+    }
 
     public void OpenUis(bool openUi)
     {
         image1.SetBool("isHidden", openUi);
         title.SetBool("isHidden", openUi);
-        tvFace.SetBool("isHidden", openUi);
+        tvFrame.SetBool("isHidden", openUi);
         image2.SetBool("isHidden", openUi);
         startBtn.SetBool("isHidden", openUi);
     }
+    public void OpenStageUI(GameObject targetParent)
+    {
+        foreach (GameObject parent in stageUIList)
+        {
+            Animator[] childAnimators = parent.GetComponentsInChildren<Animator>(true);
+            bool isTarget = (parent == targetParent);
+
+            foreach (Animator anim in childAnimators)
+            {
+                if (HasParameter(anim, "isHidden"))
+                    anim.SetBool("isHidden", !isTarget);
+            }
+        }
+    }
+
+    public void CloseAllStageUI()
+    {
+        foreach (GameObject parent in stageUIList)
+        {
+            Animator[] childAnimators = parent.GetComponentsInChildren<Animator>(true); // 자식 포함
+            foreach (Animator anim in childAnimators)
+            {
+                if (HasParameter(anim, "isHidden"))
+                    anim.SetBool("isHidden", true);
+            }
+        }
+    }
+    private bool HasParameter(Animator anim, string paramName)
+    {
+        foreach (var param in anim.parameters)
+        {
+            if (param.name == paramName) return true;
+        }
+        return false;
+    }
+
 }
 
