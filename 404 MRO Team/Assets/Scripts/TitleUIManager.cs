712308178734d00463b65fd1e0467f74e380cd89
Manje0304 <rhakswp1@gmail.com>
2025-05-19 16:17:24 +0900

    Title 씬

--- 404 MRO Team/Assets/Scripts/TitleUIManager.cs
+++ 404 MRO Team/Assets/Scripts/TitleUIManager.cs
@@ -1,23 +1,68 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 
 public class TitleUIManager : MonoBehaviour
 {
-    public Animator pannel;
+    public List<GameObject> panelList; // HGPanel, STPanel 등을 등록
+    public Animator panelAnimator;
 
-    public void OpenPannel(bool pn)
+    public List<GameObject> dlgList; // Settings Dlg 리스트
+
+    private void Start()
     {
-        pannel.SetBool("isHidden", pn);
+        SelectSettingsTab(dlgList[0]);
     }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            OpenPannel(true); // 무조건 true로 설정
+            HideAllPanels();
+        }
+    }
+
+    // 특정 패널만 보여주고 나머지는 숨김
+    public void OpenPannelUI(GameObject targetPanel)
+    {
+        foreach (GameObject panel in panelList)
+        {
+            bool isTarget = (panel == targetPanel);
+            panel.SetActive(isTarget);
+
+            Animator anim = panel.GetComponent<Animator>();
+            if (anim != null)
+            {
+                anim.SetBool("isHidden", !isTarget);
+            }
+        }
+    }
+
+    public void HideAllPanels()
+    {
+        foreach (GameObject panel in panelList)
+        {
+            Animator anim = panel.GetComponent<Animator>();
+            if (anim != null)
+            {
+                anim.SetBool("isHidden", true);
+            }
+        }
+    }
+    public void ClosePanel(GameObject targetPanel)
+    {
+        Animator anim = targetPanel.GetComponent<Animator>();
+        if (anim != null)
+        {
+            anim.SetBool("isHidden", true);
+        }
+    }
+
+    public void SelectSettingsTab(GameObject targetDlg)
+    {
+        foreach (GameObject dlg in dlgList)
+        {
+            dlg.SetActive(dlg == targetDlg);
         }
     }
 
